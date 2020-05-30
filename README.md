11972ff1e038f887784de45dd889a4a651b2e895
DittoGod <dittogod1@gmail.com>
2020-05-30 20:26:09 +0100

    updating README.md

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Enigma
 The Enigma Machine coded in java
+
+![Enigma Machine](/images/Enigma_Machine.png)
--- src/test/java/com/dittogod/enigma/enigma/DemoApplicationTests.java
+++ src/test/java/com/dittogod/enigma/enigma/DemoApplicationTests.java
@@ -1,13 +1,13 @@
-package com.dittogod.enigma.enigma;
+// package com.dittogod.enigma.enigma;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DemoApplicationTests {
+// @SpringBootTest
+// class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
